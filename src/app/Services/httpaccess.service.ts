1cafb76495eee8ba089285349deb22140b476cfa
Nuwan <nuwansampath2007@gmail.com>
2023-06-09 16:26:38 +0100

    commit 5

--- src/app/Services/httpaccess.service.ts
+++ src/app/Services/httpaccess.service.ts
@@ -11,8 +11,6 @@ export class HttpaccessService {
   private apiUrl = 'https://remote.northwoodtechnology.ie:8443/ProductsWebOnly'; // Replace with your API endpoint URL
   private token = 'MThkZDgzMzktMWE4ZC00MTA5LTkzNj'; // Replace with your bearer token
 
-
-
   constructor(private http: HttpClient) { }
 
 //   getPosts(): Observable<any[]> {
@@ -31,16 +29,16 @@ export class HttpaccessService {
 //       );
 // }
 
-getPosts(): Observable<any[]> {
+getPosts(): Observable<any> {
   // const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
 
   const headers = new HttpHeaders({
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${this.token}`,
-    // 'Access-Control-Allow-Origin': '*',
-    // 'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, PUT, OPTIONS',
-    // 'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
-    // 'Access-Control-Allow-Credentials': 'false',
+    'Access-Control-Allow-Origin': 'http://localhost:4200/',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE, PUT, OPTIONS',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Credentials': 'true',
     // 'Access-Control-Max-Age': '86400',
     // 'Access-Control-Expose-Headers': 'Content-Length, X-JSON',
   });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,39 @@ import { HttpaccessService } from './Services/httpaccess.service';
 })
 export class AppComponent {
   title = 'NW-APP';
-
-  data1: any[] = [];
+  DataArray: any;
 
   constructor(private apiService: HttpaccessService) { }
 
   ngOnInit() {
-    this.apiService.getPosts().subscribe(data => {
-        console.log(data);
-        this.data1 = data;
-      });
+    this.apiService.getPosts().subscribe(data1 => {
+      this.DataArray = data1.Data;
+
+      console.log(this.DataArray);
+
+
+        // console.log(data);
+
+        // this.DataArray = data as any[];	// FILL THE ARRAY WITH DATA.
+        //   console.log("DataArray: " + this.DataArray);
+        // this.DataArray = data;
+          // this.DataArray = Array.of(data);	// FILL THE ARRAY WITH DATA.
+          // this.DataArray = Object.values(data1);
+          // this.DataArray = Object.keys(data1);
+          // this.DataArray = Object.entries(data1);
+
+          // this.DataArray.forEach(element => {
+
+          //   this.DataArray2.push(element[1]);
+          //   console.log(element[1]);
+          // });
+
+          // console.log(this.DataArray);
+
+          // console.log("DataArray: " + this.DataArray);
+          // console.log("DataArray: " + this.DataArray[0]);
+          // console.log("DataArray: " + this.DataArray[1].Data);
+    });
 
   }
 }
